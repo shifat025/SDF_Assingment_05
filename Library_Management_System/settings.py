356b17d4f1ab9d2af53e233a9157c50814c2fbe5
Shifat-project <shifat32153@gmail.com>
2024-01-12 19:26:14 +0600

    second commit

--- Library_Management_System/settings.py
+++ Library_Management_System/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://my-library1.onrender.com','https://*.127.0.0.1']
 
 # Application definition
 
