19a9e9672e26378d277578b579d6a0171ca43e16
Shifat-project <shifat32153@gmail.com>
2024-01-16 12:43:27 +0600

    Review

--- Library_Management_System/views.py
+++ Library_Management_System/views.py
@@ -3,7 +3,8 @@ from Book.models import Books,Book_category
 from django.views.generic import DetailView
 from Borrowbook.forms import ReviewForm
 from Borrowbook.views import borrow
-
+from Borrowbook.models import Borrowedbook
+from django.http import HttpResponseForbidden
 
 def home(request, book_slug = None):
     data = Books.objects.all()
@@ -14,28 +15,42 @@ def home(request, book_slug = None):
     return render(request, 'home.html', {'data': data, 'book': book})
 
 class Details(DetailView):
-    model = Books  
+    model = Books
     pk_url_kwarg = 'id'
     template_name = 'book_details.html'
     context_object_name = 'details'
-    
-    
-    def post(self, request, *args, **kwargs):
-        review_form = ReviewForm(data=self.request.POST)
-        book_model = self.get_object()
-        if review_form.is_valid():
-            new_review = review_form.save(commit=False)
-            new_review.book = book_model
-            new_review.user = self.request.user 
-            new_review.save()
-        return self.get(request, *args, **kwargs)
-      
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         book_model = self.object
-        reviews = book_model.review_set.all()
-        review_form = ReviewForm()
-        
+
+        # Check if the user has bought the specific book
+        has_bought_book = Borrowedbook.objects.filter(user=self.request.user, books=book_model).exists()
+
+        if has_bought_book:
+            reviews = book_model.review_set.all()
+            review_form = ReviewForm()
+        else:
+            reviews = None
+            review_form = None
+
         context['reviews'] = reviews
         context['review_form'] = review_form
+        context['has_bought_book'] = has_bought_book
+
         return context
+
+    def post(self, request, *args, **kwargs):
+        book_model = self.get_object()
+
+        # Check if the user has bought the specific book
+        if not Borrowedbook.objects.filter(user=request.user, books=book_model).exists():
+            return HttpResponseForbidden("You can only review books you have bought.")
+
+        review_form = ReviewForm(data=self.request.POST)
+        if review_form.is_valid():
+            new_review = review_form.save(commit=False)
+            new_review.book = book_model
+            new_review.user = self.request.user
+            new_review.save()
+        return self.get(request, *args, **kwargs)
